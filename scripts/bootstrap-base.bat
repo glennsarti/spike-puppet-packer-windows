62b123e2296f7920eeeece61897dfadba23f6ef6
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-01-15 17:09:38 -0800

    More commits.  The shutdown-packer file is causing packer to error.  Looks like bad powerrshell.

--- scripts/bootstrap-base.bat
+++ scripts/bootstrap-base.bat
@@ -7,7 +7,7 @@ SET LOGFILE=%WINDIR%\TEMP\bootstrap-base.bat.log
 ECHO Script Started >> %LOGFILE%
 
 ECHO Setting PS Execution policy to RemoteSigned (Native) >> %LOGFILE%
-C:\Windows\cmd.exe /c powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force" >> %LOGFILE%
+powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force" >> %LOGFILE%
 
 ECHO Setting PS Execution policy to RemoteSigned (32bit on 64bit OS) >> %LOGFILE%
 C:\Windows\SysWOW64\cmd.exe /c powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force" >> %LOGFILE%
--- scripts/shutdown-packer.bat
+++ scripts/shutdown-packer.bat
@@ -1,2 +1,7 @@
-netsh advfirewall firewall set rule name="WinRM-HTTP" new action=block
-shutdown /s /t 10 /f /d p:4:1 /c "Packer Shutdown"
+REM shutdown /s /t 60 /f /d p:4:1 /c "Packer Shutdown"
+
+powershell "& { Start-Sleep -Seconds 5; Stop-Service Winrm -ErrorAction SilentlyContinue; (& netsh advfirewall firewall set rule name=WinRM-HTTP new action=block) }"
+
+REM netsh advfirewall firewall set rule name="WinRM-HTTP" new action=block
+
+EXIT /B 0
